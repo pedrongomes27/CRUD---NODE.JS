3d52098832557c7106c32a5789206b3be3636963
pedrongomes27 <pedro.ngomes27@hotmail.com>
2023-03-13 15:26:45 -0300

    CSS

--- script.js
+++ script.js
@@ -1,80 +1,51 @@
 const list = document.getElementById('list');
 const addButton = document.getElementById('add-button');
 const saveButton = document.getElementById('save-button');
+
 const apiUrl = 'http://localhost:3000/tasks';
-
-// função para criar um elemento de lista
-function createListItem(item, index) {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
-    <div class="task-item">
-      <h3>${item.title}</h3>
-      <p>${item.description}</p>
-      <p>${item.priority}</p>
-      <p>${item.dueDate}</p>
-      <div class="button-group">
-        <button class="edit-button" onclick="editItem(${index})">Editar</button>
-        <button class="delete-button" onclick="deleteItem(${index})">Excluir</button>
-      </div>
-    </div>
-  `;
-    return listItem;
-}
-
-// função para renderizar a lista de tarefas
-function renderList() {
-    // limpa a lista existente
-    list.innerHTML = '';
-
-    // faz uma solicitação GET para a rota /tasks do servidor
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // cria um elemento de lista para cada item na lista de tarefas
-            data.forEach((item, index) => {
-                const listItem = createListItem(item, index);
-                list.appendChild(listItem);
-            });
-        })
-        .catch(error => console.log(error));
-}
 
 // função para adicionar um item à lista de tarefas
 function addItem() {
-    const titleInput = document.getElementById('title');
-    const descriptionInput = document.getElementById('description');
-    const priorityInput = document.getElementById('priority');
-    const dueDateInput = document.getElementById('due-date');
+    const titleInput = document.getElementById('title').value;
+    const descriptionInput = document.getElementById('description').value;
+    const priorityInput = document.getElementById('priority').value;
+    const dueDateInput = document.getElementById('due-date').value;
 
-    const newItem = {
-        title: titleInput.value,
-        description: descriptionInput.value,
-        priority: priorityInput.value,
-        dueDate: dueDateInput.value,
-    };
+    if (titleInput.trim() !== '') {
+        const newItem = {
+            title: titleInput,
+            description: descriptionInput,
+            priority: priorityInput,
+            dueDate: dueDateInput,
+        };
 
-    // faz uma solicitação POST para a rota /tasks do servidor
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newItem),
-    })
-        .then(response => response.json())
-        .then(data => {
-            // atualiza a lista de tarefas
-            renderList();
-            // limpa os campos do formulário
-            titleInput.value = '';
-            descriptionInput.value = '';
-            priorityInput.value = '';
-            dueDateInput.value = '';
+        // faz uma solicitação POST para a rota /tasks do servidor
+        fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newItem),
         })
-        .catch(error => console.log(error));
+            .then(response => response.json())
+            .then(data => {
+                // atualiza a lista de tarefas
+                renderList();
+                // limpa os campos do formulário
+                document.getElementById('title').value = '';
+                document.getElementById('description').value = '';
+                document.getElementById('priority').value = '';
+                document.getElementById('due-date').value = '';
+            })
+            .catch(error => console.log(error));
+    } else {
+        // Exibe uma mensagem de erro ou destaca o campo de título para o usuário
+        alert('Por favor insira um título');
+    }
+
+
 }
 
-// função para atualizar um item na lista de tarefas
 function updateItem(index) {
     const titleInput = document.getElementById('title');
     const descriptionInput = document.getElementById('description');
@@ -88,8 +59,10 @@ function updateItem(index) {
         dueDate: dueDateInput.value,
     };
 
+    const itemId = JSON.parse(localStorage.getItem(`todoItem-${index}`)).id;
+
     // faz uma solicitação PUT para a rota /tasks/:id do servidor
-    fetch(`${apiUrl}/${index}`, {
+    fetch(`${apiUrl}/${itemId}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
@@ -98,6 +71,8 @@ function updateItem(index) {
     })
         .then(response => response.json())
         .then(data => {
+            // atualiza o item no localStorage
+            localStorage.setItem(`todoItem-${index}`, JSON.stringify(data));
             // atualiza a lista de tarefas
             renderList();
             // limpa os campos do formulário
@@ -109,22 +84,16 @@ function updateItem(index) {
             addButton.style.display = 'inline-block';
             saveButton.style.display = 'none';
         })
-        .catch(error => console.log(error));
+        .catch(error => console.error(error));
 }
 
-// função para editar um item na lista de tarefas
-function editItem(index) {
-    const item = todoList[index];
-    const titleInput = document.getElementById('title');
-    const descriptionInput = document.getElementById('description');
-    const priorityInput = document.getElementById('priority');
-    const dueDateInput = document.getElementById('due-date');
 
-    // preenche os campos do formulário com os dados do item selecionado
-    titleInput.value = item.title;
-    descriptionInput.value = item.description;
-    priorityInput.value = item.priority;
-    dueDateInput.value = item.dueDate;
+function editItem(index) {
+    const item = JSON.parse(localStorage.getItem(`todoItem-${index}`));
+    document.getElementById('title').value = item.title;
+    document.getElementById('description').value = item.description;
+    document.getElementById('priority').value = item.priority;
+    document.getElementById('due-date').value = item.dueDate;
 
     // alterna os botões entre adicionar e salvar
     addButton.style.display = 'none';
@@ -149,23 +118,37 @@ function deleteItem(index) {
 // função para criar um elemento de lista para um item na lista de tarefas
 function createListItem(item, index) {
     const listItem = document.createElement('li');
-    listItem.innerHTML = `
-    
+    const checkboxHTML = `
+    <input class="task-checkboxas priority-${item.priority}" type="checkbox" onchange="localStorage.setItem('todoItem-${index}-checked', this.checked)" ${localStorage.getItem(`todoItem-${index}-checked`) === 'true' ? 'checked' : ''}>
+    `;
+
+    const taskHTML = `
     <div class="div-js">
-        <h3>${item.title}</h3>
-        <div class="buttons">
-            <button class="edit-button" onclick="editItem(${index})">Editar</button>
-            <button class="delete-button" onclick="deleteItem(${index})">Excluir</button>
-        </div>
+        ${checkboxHTML}
+        <h2 class="task-title">${item.title}</h2>
+        <p class="task-due-date">${item.dueDate}</p>
+        <p class="task-priority priority-${item.priority}"></p>
     </div>
-    <p>${item.description}</p>
+    <p class="task-description">${item.description}</p>
     <div class="div-js">
-        <span class="task-priority">${item.priority}</span>
-        <span class="task-due-date">${item.dueDate}</span>
+        <button class="option-button" id="editButton" onclick="editItem(${index})">
+            <img src="img/icon-edit.png">
+            <span>Editar</span>
+        </button>
+        <button class="option-button" id="deleteButton" onclick="deleteItem(${index})">
+            <img src="img/icon-delete.png">
+            <span>Deletar</span>
+        </button>
     </div>
     `;
+
+    const liHTML = `<li>${taskHTML}</li>`;
+
+    listItem.innerHTML += liHTML;
+
     return listItem;
 }
+
 // função para renderizar a lista de tarefas
 function renderList() {
     // limpa a lista existente
